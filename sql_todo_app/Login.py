8f6321881331afaaf6d6700c8f39e0330522f76e
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-09 01:48:42 +0300

    Authentication for user is added

--- sql_todo_app/Login.py
+++ sql_todo_app/Login.py
@@ -1,52 +1,91 @@
-from fastapi import FastAPI
-from fastapi_login import LoginManager
+from typing import Union
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 app = FastAPI()
 
-SECRET = "super-secret-key"
-manager = LoginManager(SECRET, '/login')
 
-DB = {
-    'users': {
-        'johndoe@mail.com': {
-            'name': 'John Doe',
-            'password': 'hunter2'
-        }
-    }
-}
-
-@manager.user_loader()
-def query_user(user_id: str):
-    """
-    Get a user from the db
-    :param user_id: E-Mail of the user
-    :return: None or the user object
-    """
-    return DB['users'].get(user_id)
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
 
-from fastapi import Depends
-from fastapi.security import OAuth2PasswordRequestForm
-from fastapi_login.exceptions import InvalidCredentialsException
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@app.post('/login')
-def login(data: OAuth2PasswordRequestForm = Depends()):
-    email = data.username
-    password = data.password
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
 
-    user = query_user(email)
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
     if not user:
-        # you can return any response or error of your choice
-        raise InvalidCredentialsException
-    elif password != user['password']:
-        raise InvalidCredentialsException
-
-    access_token = manager.create_access_token(
-    data={'sub': email})
-    return {'access_token': access_token}
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
 
 
-@app.get('/protected')
-def protected_route(user=Depends(manager)):
-    return {'user': user}
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+ # import pdb; pdb.set_trace()
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
