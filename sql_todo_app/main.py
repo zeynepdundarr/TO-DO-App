950d5c709842210e75605312659424de2a61b423
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-08 20:05:26 +0300

    old update_a_todo endpoint is modified

--- sql_todo_app/main.py
+++ sql_todo_app/main.py
@@ -1,5 +1,6 @@
 from typing import List
 from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import any_
 from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -47,6 +48,10 @@ def create_todo_for_user(
     user_id: int, todo: schemas.TodoCreate, db: Session = Depends(get_db)):
     return crud.create_user_todo(db=db, todo=todo, user_id=user_id)
 
-@app.post("/todos/{todo_id}", response_model=schemas.Todo)
-def update_a_todo(todo_id: int, db: Session = Depends(get_db)):
-    return crud.update_a_todo(db, todo_id=todo_id)
+# @app.post("/todos/{todo_id}/", response_model=schemas.Todo)
+# def update_a_todo(todo_id: int, feature, new_val, db: Session = Depends(get_db)):
+#     return crud.update_a_todo(db, feature, new_val, todo_id=todo_id)
+
+@app.patch("/todos/{todo_id}", response_model=schemas.Todo)
+def update_todo(todo_id: int, todo: schemas.TodoUpdate):
+    return crud.update_a_todo(todo_id, todo)
