7bb095ef28f521c416f69bca5d9b65a76ea73e26
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-08 23:29:06 +0300

    Login returns success for sample user

--- sql_todo_app/Login.py
+++ sql_todo_app/Login.py
@@ -23,3 +23,23 @@ def query_user(user_id: str):
     :return: None or the user object
     """
     return DB['users'].get(user_id)
+
+
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_login.exceptions import InvalidCredentialsException
+
+
+@app.post('/login')
+def login(data: OAuth2PasswordRequestForm = Depends()):
+    email = data.username
+    password = data.password
+
+    user = query_user(email)
+    if not user:
+        # you can return any response or error of your choice
+        raise InvalidCredentialsException
+    elif password != user['password']:
+        raise InvalidCredentialsException
+
+    return {'status': 'Success'}
