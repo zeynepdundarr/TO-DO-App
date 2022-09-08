f41850a5030aaaa3f299529ee29376485ac76ddc
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-08 21:37:16 +0300

    Login.py is set up

---
+++ sql_todo_app/Login.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi_login import LoginManager
+
+app = FastAPI()
+
+SECRET = "super-secret-key"
+manager = LoginManager(SECRET, '/login')
+
+DB = {
+    'users': {
+        'johndoe@mail.com': {
+            'name': 'John Doe',
+            'password': 'hunter2'
+        }
+    }
+}
+
+@manager.user_loader()
+def query_user(user_id: str):
+    """
+    Get a user from the db
+    :param user_id: E-Mail of the user
+    :return: None or the user object
+    """
+    return DB['users'].get(user_id)
