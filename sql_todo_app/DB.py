ca0316209a5a9c99f8c07d28ee33c9ced8b3ff78
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-09 13:56:48 +0300

    DB.file created

--- sql_todo_app/DB.py
+++ sql_todo_app/DB.py
@@ -1,20 +1,9 @@
+from sqlalchemy.orm import Session
 from .database import SessionLocal, engine
 
-class DB:
-
-    db = None 
-
-    def __init__(self):
-        print("Init shared db")
-
-    def init_db(self):
-        self.db = SessionLocal()
-        print("I am instance {0}".format(self))
-        try:
-            # yield'ın kalması mantıklı mı
-            yield self.db
-        finally:
-            self.db.close()
-
-    def get_db(self):
-        return self.db
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
