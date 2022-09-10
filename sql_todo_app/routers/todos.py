0d9f6f6e906cc74c614f9d2d2643f56f4822cd95
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-10 17:07:59 +0300

    deleting unncessary comments

--- sql_todo_app/routers/todos.py
+++ sql_todo_app/routers/todos.py
@@ -1,27 +1,30 @@
 from fastapi import APIRouter, Depends, HTTPException
-from . import crud, schemas
+from ..crud import get_user_a_todo, get_user_todos, create_user_todo, update_a_todo
+from ..models import *
+from ..schemas import Todo, TodoUpdate, TodoCreate
 from sqlalchemy.orm import Session
-from . import DB
+from ..DB import get_db
 from typing import List
+from ..dependencies import get_token_header
 
-router = APIRouter()
+router = APIRouter(prefix="/todos", tags=["todos"], dependencies=[Depends(get_token_header)], responses={404: {"description" : "Not found"}})
 
-@router.get("/todos/{todo_id}", response_model=schemas.Todo)
-def get_a_todo(todo_id=int, db: Session = Depends(DB.get_db)):
-    return crud.get_user_a_todo(db=db, todo_id=todo_id)
+@router.get("/todos/{token}/{todo_id}", response_model=Todo)
+def get_a_todo(todo_id=int, db: Session = Depends(get_db)):
+    return get_user_a_todo(db=db, todo_id=todo_id)
 
-@router.get("/user/{user_id}/todos/", response_model=List[schemas.Todo])
-def get_todos_for_user(user_id: int, db: Session = Depends(DB.get_db)):
-    return crud.get_user_todos(db=db, user_id=user_id)
+@router.get("/user/{user_id}/todos/", response_model=List[Todo])
+def get_todos_for_user(user_id: int, db: Session = Depends(get_db)):
+    return get_user_todos(db=db, user_id=user_id)
 
 # /todos/create
-@router.post("/users/{user_id}/todos", response_model=schemas.Todo)
+@router.post("/users/{user_id}/todos", response_model=Todo)
 def create_todo_for_user(
-    user_id: int, todo: schemas.TodoCreate, db: Session = Depends(DB.get_db)):
-    return crud.create_user_todo(db=db, todo=todo, user_id=user_id)
+    user_id: int, todo: TodoCreate, db: Session = Depends(get_db)):
+    return create_user_todo(db=db, todo=todo, user_id=user_id)
 
 # /todos/{todo_id}/modify
-@router.patch("/todos/{todo_id}", response_model=schemas.Todo)
-def update_todo(todo_id: int, todo: schemas.TodoUpdate, db: Session = Depends(DB.get_db)):
-    return crud.update_a_todo(db=db, todo=todo, todo_id=todo_id)
+@router.patch("/todos/{todo_id}", response_model=Todo)
+def update_todo(todo_id: int, todo: TodoUpdate, db: Session = Depends(get_db)):
+    return update_a_todo(db=db, todo=todo, todo_id=todo_id)
 
--- sql_todo_app/routers/users.py
+++ sql_todo_app/routers/users.py
@@ -12,6 +12,7 @@ router = APIRouter()
 @router.post("/users/", tags=["users"], response_model=User)
 def register(user: UserCreate, db: Session = Depends(get_db)):
     db_user = get_user_by_email(db, email=user.email)
+    
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
 
