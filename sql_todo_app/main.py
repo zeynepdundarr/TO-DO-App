c0a362fb63db71fc2299b63fe4041622b343a31a
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-10 17:07:24 +0300

    login.py is added as a router to main.py, main.py is modified

--- sql_todo_app/main.py
+++ sql_todo_app/main.py
@@ -1,11 +1,14 @@
 from fastapi import Depends, FastAPI
 from .dependencies import get_query_token, get_token_header
 from .internal import admin
-from .routers import users, todos
+from .routers import users, todos, login
 
 app = FastAPI()
 app.include_router(users.router)
 app.include_router(todos.router)
+app.include_router(login.router)
+
+
 app.include_router(
     admin.router,
     prefix="/admin",
