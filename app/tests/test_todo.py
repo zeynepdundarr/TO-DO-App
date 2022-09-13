03b3c0d356c1049ba051bef27c47b94aba78f3d5
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-13 16:12:29 +0300

    test_get_a_todo passed

--- app/tests/test_todo.py
+++ app/tests/test_todo.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from ..main import app
 from ..DB import get_db
 from ..database import Base
-
+from ..crud import get_user
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
@@ -18,18 +18,7 @@ Base.metadata.create_all(bind=engine)
 username = "Zeynep53"
 password = username
 email = username+"@example.com"
-a_user_json = {"email": email, 
-                "username": username,
-                "password": password}
 
-user_form_data = {"grant_type": "password",
-                      "username": username,
-                      "password": password,
-                      "scope": "",
-                      "client_id": "",
-                      "client_secret": ""}
-
-todo_id = 1
 a_todo = {"title": "Test Todo",
                     "description":"Sample todo is shown.",
                     "status":"pending",
@@ -43,6 +32,13 @@ user_form_data = {"grant_type": "password",
                       "client_id": "",
                       "client_secret": ""}
 
+create_todo_header = {"accept": "application/json",
+                        "authorization": f"Bearer {username}",
+                        "content-type": "application/json"}
+
+get_todo_header = {"accept": "application/json",
+                   "authorization": f"Bearer {username}"}
+
 def override_get_db():
     try:
         db = TestingSessionLocal()
@@ -52,13 +48,14 @@ def override_get_db():
 
 app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
+client.post("/token", data=user_form_data, headers={"content-type": "application/x-www-form-urlencoded"})
 
 def test_create_todo_for_user():
-    response = client.post("/token", data=user_form_data, headers={"content-type": "application/x-www-form-urlencoded"})
-    create_todo_headers = {"accept": "application/json",
-              "Authorization": "Bearer Zeynep",
-              "Content-Type": "application/json"}
-    response = client.post("/todos/create/", json=a_todo, headers=create_todo_headers)
+    response = client.post("/todos/create/", json=a_todo, headers=create_todo_header)
     assert response.status_code == 201, response.text
 
-
+todo_id = 12
+def test_get_a_todo():
+    client.post("/todos/create/", json=a_todo, headers=create_todo_header)
+    response = client.get(f"/todos/{todo_id}", headers=get_todo_header)
+    assert response.status_code == 200, response.text
