10af4a3e9a07eaa27bb3e579b9f07ee62fd75f0b
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-13 13:45:54 +0300

    test_read_user_me passed the test

--- app/test_user.py
+++ app/test_user.py
@@ -2,10 +2,11 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from app.login import fake_decode_token
+from app.login import fake_decode_token, get_current_active_user
 from .main import app
 from .DB import get_db
 from .database import Base
+from .crud import get_user
 
 from fastapi import FastAPI, Depends
 from .schemas import UserCreate
@@ -24,12 +25,19 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engin
 
 Base.metadata.create_all(bind=engine)
 
-username = "Zeynep52"
+username = "Zeynep54"
 password = username
 email = username+"@example.com"
 a_user_json = {"email": email, 
                 "username": username,
                 "password": password}
+
+user_form_data = {"grant_type": "password",
+                      "username": username,
+                      "password": password,
+                      "scope": "",
+                      "client_id": "",
+                      "client_secret": ""}
 
 def override_get_db():
     try:
@@ -41,20 +49,17 @@ def override_get_db():
 app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
-# def test_register():
-#     response = client.post("/users/", json=a_user_json)
-#     assert response.status_code == 201, response.text
-
+def test_register():
+    response = client.post("/users/", json=a_user_json)
+    assert response.status_code == 201, response.text
 
 def test_user_login():
-
-    user_form_data = {"grant_type": "password",
-                      "username": username,
-                      "password": password,
-                      "scope": "",
-                      "client_id": "",
-                      "client_secret": ""}
     response = client.post("/token", data=user_form_data, headers={"content-type": "application/x-www-form-urlencoded"})
-    print("Test 1 - response.json: ", response.json)
-    response.json
     assert response.status_code == 200, response.text
+
+def test_read_user_me():
+    client.post("/token", data=user_form_data)
+    response = client.get("/users/me", headers={"Authorization": f"Bearer {user_form_data['username']}", "accept": "application/json"})
+    assert response.status_code == 200, response.text
+
+
