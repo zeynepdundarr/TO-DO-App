cfc7a1bfdabbdfe0a18b319ef1a289bdbe77f47f
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-16 16:49:21 +0300

    test_filter_todos check response data type is checked

--- app/tests/test_todo.py
+++ app/tests/test_todo.py
@@ -40,28 +40,25 @@ def clean_db():
     client.delete("/todos/delete/all/", headers=Constants.authentication_header)
     client.delete("/users/delete/all/")
 
-# def test_create_todo_for_user():
-#     set_db()
-#     response = client.post("/todos/create/", json=Constants.a_todo_1, headers=Constants.create_todo_header)
-#     print("Test - 1:", response.json())
-#     clean_db()
-#     assert response.status_code == 201, response.text
-#     assert response.json() == Constants.a_todo_1
+def test_create_todo_for_user():
+    set_db()
+    response = client.post("/todos/create/", json=Constants.a_todo_1, headers=Constants.create_todo_header)
+    clean_db()
+    assert response.status_code == 201, response.text
+    assert response.json() == Constants.a_todo_1
 
-# def test_get_a_todo():
-#     set_db()
-#     client.post("/todos/create/", json=Constants.a_todo_1, headers=Constants.create_todo_header)
-#     response = client.get(f"/todos/{Constants.todo_id}", headers=Constants.authentication_header)
-#     clean_db()
-#     assert response.status_code == 200, response.text    
-#     assert response.json() == Constants.a_todo_1
+def test_get_a_todo():
+    set_db()
+    client.post("/todos/create/", json=Constants.a_todo_1, headers=Constants.create_todo_header)
+    response = client.get(f"/todos/{Constants.todo_id}", headers=Constants.authentication_header)
+    clean_db()
+    assert response.status_code == 200, response.text    
+    assert response.json() == Constants.a_todo_1
 
 def test_get_todos_for_user():
-
     set_db()
     # number of todos will be added during test
     no_of_todos = 3
-
     client.post("/todos/create/", json=Constants.a_todo_1, headers=Constants.create_todo_header)
     client.post("/todos/create/", json=Constants.a_todo_2, headers=Constants.create_todo_header)
     client.post("/todos/create/", json=Constants.a_todo_3, headers=Constants.create_todo_header)
@@ -79,16 +76,21 @@ def test_get_todos_for_user():
             assert todo == Constants.a_todo_2
         elif todo["id"] == 3:
             assert todo == Constants.a_todo_3
-
     assert response.status_code == 200, response.text
 
-# def test_filter_todos():
-#     set_db()
-#     field = "category_label"
-#     value = "self"
-#     response = client.get(f"/todos/filter/{field}/{value}", headers=Constants.authentication_header)
-#     assert response.status_code == 200, response.text
+def test_filter_todos():
+    set_db()
+    create_multiple_todos()
+    field_arr = [ "status", "schedule", "is_ticked"]
+    value_arr = ["done", "today", "True"]
 
+    for field, value in zip(field_arr, value_arr):
+        response = client.get(f"/todos/filter/{field}/{value}", headers=Constants.authentication_header)
+        assert response.status_code == 200, response.text
+        for obj in response.json():
+            assert str(obj[field]) == value
+    clean_db()
+    
 # def test_modify_field():
 #     set_db()
 #     field = "category_label"
@@ -116,3 +118,7 @@ def test_get_todos_for_user():
 #     assert response.status_code == 200, response.text
 
 
+def create_multiple_todos():
+    client.post("/todos/create/", json=Constants.a_todo_1, headers=Constants.create_todo_header)
+    client.post("/todos/create/", json=Constants.a_todo_2, headers=Constants.create_todo_header)
+    client.post("/todos/create/", json=Constants.a_todo_3, headers=Constants.create_todo_header)
