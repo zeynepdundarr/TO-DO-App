d8bf7411ed8df557a67278b709de8fad2e322d98
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-16 19:09:59 +0300

    Constants.py are modified

--- app/tests/Constants.py
+++ app/tests/Constants.py
@@ -1,8 +1,15 @@
 class Constants:
-        username = "Zeynep"
-        password = username
-        email = username+"@example.com"
+        username_1 = "Zeynep"
+        username_2 = "Ali"
+        username_3 = "Zeynep"
+
+        password = username_1
+        email_1 = username_1 + "@example.com"
+        email_2 = username_2 + "@example.com"
+        email_3 = username_2 + "@example.com"
+
         todo_id = 1
+        modify_todo_id = 3
         a_todo_1 = {'category_label': 'self',
                 'description': 'Sample todo is shown.',
                 'id':1,
@@ -11,7 +18,7 @@ class Constants:
                 'owner_id': 1,
                 'priority': '',
                 'schedule': '',
-                'status': 'pending',
+                'status': 'done',
                 'title': 'Test Todo'}
 
         a_todo_2 = {'category_label': 'self',
@@ -21,35 +28,57 @@ class Constants:
                 'is_ticked': False,
                 'owner_id': 1,
                 'priority': '',
-                'schedule': '',
-                'status': 'pending',
+                'schedule': 'today',
+                'status': 'done',
                 'title': 'Test Todo'}
         
-        a_todo_3 = {'category_label': 'self',
+        a_todo_3 = {'category_label': 'home',
                 'description': 'Sample todo is shown.',
                 'id':3,
                 'is_starred': False,
-                'is_ticked': False,
+                'is_ticked': True,
                 'owner_id': 1,
                 'priority': '',
                 'schedule': '',
                 'status': 'pending',
                 'title': 'Test Todo'}
 
-        a_user_json = {"email": email, 
-                        "username": username,
+        a_user_json_1 = {"email": email_1, 
+                        "username": username_1,
                         "password": password}
 
-        user_form_data = {"grant_type": "password",
-                        "username": username,
+        a_user_json_2 = {"email": email_2, 
+                        "username": username_2,
+                        "password": password}
+
+        a_user_json_3 = {"email": email_3, 
+                        "username": username_3,
+                        "password": password}
+
+        user_form_data_1 = {"grant_type": "password",
+                        "username": username_1,
+                        "password": password,
+                        "scope": "",
+                        "client_id": "",
+                        "client_secret": ""}
+
+        user_form_data_2 = {"grant_type": "password",
+                        "username": username_2,
+                        "password": password,
+                        "scope": "",
+                        "client_id": "",
+                        "client_secret": ""}
+
+        user_form_data_3 = {"grant_type": "password",
+                        "username": username_3,
                         "password": password,
                         "scope": "",
                         "client_id": "",
                         "client_secret": ""}
 
         create_todo_header = {"accept": "application/json",
-                                "authorization": f"Bearer {username}",
+                                "authorization": f"Bearer {username_1}",
                                 "content-type": "application/json"}
 
         authentication_header = {"accept": "application/json",
-                                "Authorization": f"Bearer {username}"}
+                                "Authorization": f"Bearer {username_1}"}
