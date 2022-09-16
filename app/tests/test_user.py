3ed21e789634c58a0cdf08d1ef917add39c09eb9
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-16 19:09:38 +0300

    user tests modified

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from ..main import app
 from ..DB import get_db
 from ..database import Base
+from app.tests.Constants import Constants
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
@@ -13,23 +14,6 @@ engine = create_engine(
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base.metadata.create_all(bind=engine)
-
-username = "Zeynep57"
-password = username
-email = username+"@example.com"
-a_user_json = {"email": email, 
-                "username": username,
-                "password": password}
-
-user_form_data = {"grant_type": "password",
-                      "username": username,
-                      "password": password,
-                      "scope": "",
-                      "client_id": "",
-                      "client_secret": ""}
-
-authentication_header = {"accept": "application/json",
-                        "Authorization": f"Bearer {username}"}
 
 def override_get_db():
     try:
@@ -42,25 +26,43 @@ app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
 def clean_db():
-    client.delete("/todos/delete/all/", headers=authentication_header)
+    client.delete("/todos/delete/all/", headers=Constants.authentication_header)
     client.delete("/users/delete/all/")
 
 def test_register():
-    response = client.post("/users/", json=a_user_json)
+    response = client.post("/users/", json=Constants.a_user_json_1)
     clean_db()
     assert response.status_code == 201, response.text
 
 def test_user_login():
-    response = client.post("/users/", json=a_user_json)
-    response = client.post("/token", data=user_form_data, headers={"content-type": "application/x-www-form-urlencoded"})
+    response = client.post("/users/", json=Constants.a_user_json_1)
+    response = client.post("/token", data=Constants.user_form_data_1, headers={"content-type": "application/x-www-form-urlencoded"})
+    response = client.get("/users/me", headers={"Authorization": f"Bearer {Constants.user_form_data_1['username']}", "accept": "application/json"})
     clean_db()
-    assert response.status_code == 200, response.text
+    assert response.json()["id"] == 1
 
 def test_read_user_me():
-    response = client.post("/users/", json=a_user_json)
-    client.post("/token", data=user_form_data, headers={"content-type": "application/x-www-form-urlencoded"})
-    response = client.get("/users/me", headers={"Authorization": f"Bearer {user_form_data['username']}", "accept": "application/json"})
+    response = client.post("/users/", json=Constants.a_user_json_1)
+    client.post("/token", data=Constants.user_form_data_1, headers={"content-type": "application/x-www-form-urlencoded"})
+    response = client.get("/users/me", headers={"Authorization": f"Bearer {Constants.user_form_data_1['username']}", "accept": "application/json"})
     clean_db()
-    assert response.status_code == 200, response.text
+    assert response.json()["id"] == 1
 
+def test_authentication():
+    client.post("/users/", json=Constants.a_user_json_1)
+    # login user without authentication
+    response = client.get("/users/me", headers={"accept": "application/json"})
+    clean_db()
+    assert response.json()["detail"] == 'Not authenticated'
 
+def test_existing_username_error():
+    client.post("/users/", json=Constants.a_user_json_1)
+    response = client.post("/users/", json=Constants.a_user_json_1)
+    clean_db()
+    assert response.json()["detail"] == "Email already registered"
+    
+def test_existing_username_error():
+    client.post("/users/", json=Constants.a_user_json_1)
+    response = client.post("/users/", json=Constants.a_user_json_3)
+    clean_db()
+    assert response.json()["detail"] == "Username already registered"
