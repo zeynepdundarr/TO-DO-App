3512606a183716666f3901789f33509673a82d9b
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-17 17:07:41 +0300

    todos.py -all- is deleted in get path operation

--- app/routers/todos.py
+++ app/routers/todos.py
@@ -1,18 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException
 from ..crud import *
 from ..models import *
-from ..schemas import Todo, TodoCreate, TodoBase
+from ..schemas import Todo, TodoCreate
 from sqlalchemy.orm import Session
-from ..DB import get_db
 from ..database import *
 from typing import List
-from ..dependencies import get_token_header
+from ..DB import get_db
 from ..login import get_current_active_user
 
-# TODO: fix it
-router = APIRouter(prefix="/todos", tags=["todos"], responses={404: {"description" : "Not found"}})
+router = APIRouter(prefix="/todos", tags=["todos"], responses={404: {"description" : "Todo not found"}})
 @router.get("/{todo_id}", response_model=Todo)
-def get_a_todo(todo_id=int, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
+def get_a_todo(todo_id:str, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     user_todo_list = get_user_todos(db=db, user_id=current_user.id)
     user_todo_list = [vars(obj) for obj in user_todo_list]
     user_todo_id_list = [x["id"] for x in user_todo_list]
@@ -21,7 +19,7 @@ def get_a_todo(todo_id=int, db: Session = Depends(get_db), current_user: User = 
     else:
         raise HTTPException(status_code=404, detail="Todo not found!")
 
-@router.get("/all/", response_model=List[Todo])
+@router.get("/", response_model=List[Todo])
 def get_todos_for_user(db: Session = Depends(get_db),
 current_user: User = Depends(get_current_active_user)):
     return get_user_todos(db=db, user_id=current_user.id)
@@ -36,23 +34,23 @@ def create_todo_for_user(todo: TodoCreate, db: Session = Depends(get_db),
 def filter_todos(field: str, value: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     return read_todo_by_filter(current_user.id, field, value, db) 
 
-@router.patch("/update/{todo_id}/{field}/{value}")
+@router.patch("/{todo_id}/update/{field}/{value}")
 def modify_field(todo_id: int, field: str, value: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     if current_user:
         return update_a_todo(todo_id, field, value, db)
 
-@router.patch("/edit/{todo_id}")
+@router.patch("/{todo_id}/edit/")
 def edit_todo_by_all_fields(todo_id: int, todo: Todo,  db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     todo.id = todo_id
     todo.owner_id = current_user.id
     return update_todo_by_all_fields(todo_id, todo, db)
 
-@router.patch("/mark_as_done/{todo_id}")
+@router.patch("/{todo_id}/mark_as_done/")
 def mark_todo_as_done(todo_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     if current_user:
         return update_a_todo(todo_id, "is_ticked", "True", db)
 
-@router.delete("/delete/{todo_id}")
+@router.delete("/{todo_id}/delete/")
 def delete_a_todo(todo_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     if current_user:
         return delete_todo(todo_id, db)
