02ab2ee03dc62f956d9d8ccb84101e2e5c2b8791
zeynepdundarr <idundar17@ku.edu.tr>
2022-09-17 23:01:11 +0300

    unnecessary comments are deleted in model.py

--- app/models.py
+++ app/models.py
@@ -20,20 +20,15 @@ class Todo(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, default="", index=True)
     description = Column(String, default="", index=True)
-    # added features
     status = Column(String, default="", index=True)
     notes = Column(String, default="", index=True)
     is_ticked = Column(Boolean, default=False, index=True)
     is_starred = Column(Boolean, default=False, index=True)
     category_label = Column(String, default="", index=True)
-    # TODO: make a date & time type
     date = Column(String, default="", index=True) 
-    # TODO: check the [String] below / implement later
-    # assigned_users = Column([String], index=True) 
     priority = Column(Integer, default=0, index=True)
     due_label = Column(String, default="", index=True)
     label_color = Column(String, default="", index=True)
     schedule = Column(String, default="", index=True)
-
     owner = relationship("User", back_populates="user_todo")
     owner_id = Column(Integer, ForeignKey("Users.id"))
