d9f588d7f4dd64d9f9d8b0f0fb85b03322f4c7fc
zeynepdundarr <40126925+zeynepdundarr@users.noreply.github.com>
2022-09-17 17:09:06 +0300

    old app is deleted

--- sql_todo_app/DB.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from .database import SessionLocal, engine
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
